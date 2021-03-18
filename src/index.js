0fcb91d1dd34bb427525e17071de116f004fdd29
Ania <candyrockstore5@gmail.com>
2021-03-18 17:54:52 +0100

    Updating files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import {
+  BrowserRouter
+} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render( <
+    BrowserRouter >
+    <
+    App / >
+    <
+    /BrowserRouter>, document.getElementById('root'));
+    //the 'root' element is coming from public/index.html  
+
+    registerServiceWorker();
